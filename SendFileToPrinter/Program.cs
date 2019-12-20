10cd986d741dda28f8b357b3db047ba3ab3a7970
Glenn Chen <glenn.chen@broadway.com.tw>
2019-12-20 11:55:53 +0800

    init commit

--- SendFileToPrinter/Program.cs
+++ SendFileToPrinter/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Printing;
+using System.IO;
+using System.Diagnostics;
 
 namespace SendFileToPrinter
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            var dialog = new OpenFileDialog() { Multiselect = false, Title = "選擇PDF", Filter = "PDF檔案(*.pdf)|*.pdf" };            
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string fileName = dialog.FileName;
+
+                //var fByte = File.ReadAllBytes(fileName);
+                //var pq = LocalPrintServer.GetDefaultPrintQueue();
+
+                //using (var job = pq.AddJob())
+                //using (var stream = job.JobStream)
+                //{
+                //    stream.Write(fByte, 0, fByte.Length);
+                //}
+
+                var info = new ProcessStartInfo(fileName) { UseShellExecute = true, Verb = "print" };
+                var process = Process.Start(info);
+
+            }
+            
         }
     }
 }
